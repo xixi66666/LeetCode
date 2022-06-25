3b7f1611f971189749feb1ee4f1f43f37d7634c7
xixixiaozi <1253324157@qq.com>
2022-06-26 00:52:29 +0800

    add 110

---
+++ src/树/leetcode_110平衡二叉树.java
@@ -0,0 +1,28 @@
+package 树;
+
+/**
+ * @program: LeetCode
+ * @description: 平衡二叉树
+ * @author: 杨晨曦
+ * @create: 2022-06-25 18:01
+ **/
+public class leetcode_110平衡二叉树 {
+
+}
+class Solution10 {
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) {
+                return true;
+            } else {
+                return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+            }
+        }
+
+        public int height(TreeNode root) {
+            if (root == null) {
+                return 0;
+            } else {
+                return Math.max(height(root.left), height(root.right)) + 1;
+            }
+        }
+}
