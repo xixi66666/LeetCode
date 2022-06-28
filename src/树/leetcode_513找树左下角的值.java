a046680f082b7bfa0ac59b67e6e1c4499bc3f1a8
xixixiaozi <1253324157@qq.com>
2022-06-28 17:27:10 +0800

    update 513 450 add 235 654

--- src/树/leetcode_513找树左下角的值.java
+++ src/树/leetcode_513找树左下角的值.java
@@ -1,6 +1,8 @@
 package 树;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -18,23 +20,24 @@ public class leetcode_513找树左下角的值 {
 
 class Solution11 {
     public int findBottomLeftValue(TreeNode root) {
-        int sum = 0;
+        int ans = 0;
+        List<TreeNode> list = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         while (!queue.isEmpty()){
             int len = queue.size();
             for(int i = 0;i<len;i++){
                 TreeNode node = queue.poll();
-                if(node.left != null){
-                    queue.add(node.left);
-
-                }
                 if(node.right != null){
                     queue.add(node.right);
                 }
-
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                ans = node.val;
             }
         }
+        return ans;
     }
 
 }
