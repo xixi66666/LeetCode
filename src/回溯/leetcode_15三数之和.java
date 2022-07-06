a48ce559b056743495bcc17c940dae5d711e21a5
xixixiaozi <1253324157@qq.com>
2022-07-06 16:44:17 +0800

    update 15

--- src/回溯/leetcode_15三数之和.java
+++ src/回溯/leetcode_15三数之和.java
@@ -19,18 +19,24 @@ class Solution9 {
         int number = nums.length;
         Arrays.sort(nums);
         for(int a = 0;a<number;a++){
+            //如果相等就跳过，在a这一层的循环上不能重复
             if(a > 0 && nums[a] == nums[a-1]){
                 continue;
             }
+            // c 对应的指针初始指向数组的最右端
             int target = -nums[a];
             int c = number-1;
             for(int b = a+1;b<number;b++){
+                //如果相等就跳过，在b这一层的循环上不能重复
                 if(b > a+1 && nums[b] == nums[b-1]){
                     continue;
                 }
+                //需要保证b在c左侧以免重复
                 while(b < c && nums[b] + nums[c] > target){
                     c--;
                 }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
                 if(b == c){
                     break;
                 }
