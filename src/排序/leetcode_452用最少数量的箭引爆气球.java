dde452c80fd64380d873802740ca2529514d8b7b
xixixiaozi <1253324157@qq.com>
2022-07-06 16:42:05 +0800

    add 15 update 452

--- src/排序/leetcode_452用最少数量的箭引爆气球.java
+++ src/排序/leetcode_452用最少数量的箭引爆气球.java
@@ -1,5 +1,8 @@
 package 排序;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class leetcode_452用最少数量的箭引爆气球 {
     public static void main(String[] args) {
 
@@ -7,6 +10,29 @@ public class leetcode_452用最少数量的箭引爆气球 {
 }
 class Solution7 {
     public int findMinArrowShots(int[][] points) {
-        return 1;
+        if(points.length == 0){
+            return 0;
+        }
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] point1, int[] point2) {
+                if(point1[1] > point2[1]){
+                    return 1;
+                }else if (point1[1] < point2[2]){
+                    return -1;
+                }else {
+                    return 0;
+                }
+            }
+        });
+        int start = points[0][1];
+        int ans = 1;
+        for(int[] p : points){
+            if(p[0] > start){
+                ans++;
+                start = p[1];
+            }
+        }
+        return ans;
     }
 }
