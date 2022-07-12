39e6b1bcaa5dc744e838ab8248faf74cbf425577
xixixiaozi <1253324157@qq.com>
2022-07-12 15:11:12 +0800

    add 31 32 48 49

---
+++ src/Hot100/leetcode_31下一个排列.java
@@ -0,0 +1,38 @@
+package Hot100;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author YangChenxi
+ */
+public class leetcode_31下一个排列 {
+    public static void main(String[] args) {
+        Solution6 solution6 = new Solution6();
+        solution6.nextPermutation(new int[]{1,2,3});
+    }
+}
+class Solution6 {
+    List<Integer> list = new ArrayList<>();
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+        for (int i = len - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                Arrays.sort(nums, i, len);
+                for (int j = i; j <len; j++) {
+                    if (nums[j] > nums[i - 1]) {
+                        int temp = nums[j];
+                        nums[j] = nums[i - 1];
+                        nums[i - 1] = temp;
+                        return;
+                    }
+                }
+            }
+        }
+        Arrays.sort(nums);
+        return;
+
+    }
+}
