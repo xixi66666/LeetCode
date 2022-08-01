d75fc9b9dd910191c074ee68ed67f9f5ebb0e1ba
YangChenxi <1253324157@qq.com>
2022-08-01 17:32:07 +0800

    add 240

---
+++ src/Hot100/leetcode_240搜索二维矩阵2.java
@@ -0,0 +1,41 @@
+package Hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author YangChenxi
+ */
+public class leetcode_240搜索二维矩阵2 {
+    public static void main(String[] args) {
+        Solution28 solution28 = new Solution28();
+        boolean b = solution28.searchMatrix(new int[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}, 19);
+        System.out.println(b);
+    }
+}
+class Solution28 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix[0].length-1;
+        List<Integer> line = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        for(int i = 0;i<matrix.length;i++){
+            if(matrix[i][left] <= target && matrix[i][right] >= target){
+                line.add(i);
+            }
+        }
+        for(int i = 0;i<matrix[0].length;i++){
+            if(matrix[0][i] <= target && matrix[matrix.length-1][i] >= target){
+                row.add(i);
+            }
+        }
+        for(int i : line){
+            for(int j : row){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
