626cc523f2671c08f1897edfdf1a317e883dfde9
YangChenxi <1253324157@qq.com>
2022-08-02 23:18:05 +0800

    add 12 13

---
+++ src/other/leetcode_12整数转罗马数字.java
@@ -0,0 +1,28 @@
+package other;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author YangChenxi
+ */
+public class leetcode_12整数转罗马数字 {
+}
+class Solution5 {
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    public String intToRoman(int num) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i = 0;i<values.length;i++){
+            int value = values[i];
+            if(num >= value){
+                num -= value;
+                stringBuffer.append(symbols[i]);
+            }
+            if(num == 0){
+                break;
+            }
+        }
+        return stringBuffer.toString();
+    }
+}
