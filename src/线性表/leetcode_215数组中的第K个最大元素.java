1371bf14f0a8ee7a3c6e7b52679a230ad205d7d3
YangChenxi <1253324157@qq.com>
2022-08-25 14:33:38 +0800

    add

---
+++ src/线性表/leetcode_215数组中的第K个最大元素.java
@@ -0,0 +1,15 @@
+package 线性表;
+
+import java.util.Arrays;
+
+/**
+ * @author YangChenxi
+ */
+public class leetcode_215数组中的第K个最大元素 {
+}
+class Solution6 {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
