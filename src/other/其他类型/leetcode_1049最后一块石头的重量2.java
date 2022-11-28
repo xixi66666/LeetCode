aace4e2cb3f90f9b435d2fc9a8b06faac671cbef
xixixiaozi <1253324157@qq.com>
2022-11-28 13:32:48 +0800

    update 1049

--- src/other/其他类型/leetcode_1049最后一块石头的重量2.java
+++ src/other/其他类型/leetcode_1049最后一块石头的重量2.java
@@ -14,10 +14,31 @@ public class leetcode_1049最后一块石头的重量2 {
 }
 class Solution13{
     public int lastStoneWeightII(int[] stones) {
-        int a = 3;
-        int b = a++ + ++a;
-        System.out.println(a);
-        System.out.println(b);
-        return b;
+        int sum = 0;
+        for(int n1 : stones){
+            sum += n1;
+        }
+        int n = stones.length;
+        int m = sum/2;
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[0][0] = true;
+        /*
+        前i个能不能凑成j的重量
+         */
+        for(int i = 0;i<=n;i++){
+            for(int j = 0;j <= m;j++){
+                if(j < stones[i]){
+                    dp[i+1][j] = dp[i][j];
+                }
+                else{
+                    dp[i+1][j] = dp[i][j] || dp[i][j - stones[i]];
+                }
+            }
+        }
+        for (int j = m;; --j) {
+            if (dp[n][j]) {
+                return sum - 2 * j;
+            }
+        }
     }
 }
